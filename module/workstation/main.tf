9c94c77ed81429e6bbd59413a553ac1181537d67
dhwarakesh <dhwarakesh31@gmail.com>
2024-12-25 18:45:54 -0500

    create setup instructions

--- module/workstation/main.tf
+++ module/workstation/main.tf
@@ -34,7 +34,7 @@ resource "null_resource" "shell" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo -i",
+      "sudo su",
       "pip3.11 install ansible",
       "labauto install gocd-agent"
     ]
